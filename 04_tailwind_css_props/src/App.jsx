39ecf170b4ac24829192aed5217ea1b50015eeec
alirazaatish <atishaliraza>
2024-08-29 10:37:46 +0500

    React Component Practice Code

---
+++ 04_tailwind_css_props/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className="bg-green-400 p-7 text-3xl font-bold rounded-xl mb-5">Tailwind Css  | Props | Chai Aur React</h1>
+    </>
+  )
+}
+
+export default App
